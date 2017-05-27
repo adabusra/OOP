1d6655de26e43499d1ed2a9c7659c89339258ee8
adabusra <busraa.adaa@gmail.com>
2017-05-27 16:46:45 +0300

    Proje eklendi. OOP anlatimina baslandi

---
+++ OOP/OOP/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Calisanlar personelform = new Calisanlar();
+
+        int sayac = 0;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Arkadaşlar nesne lazım olmasa bile üretelim. Büyük çamplı projelerde ilerleyen süreçlerde gerekli olma ihtimali yüksektir. Önemli olan projelerimizin ne kadar hızlı bir şekilde güncellenebileceği önemlidir. DEğiştirmek istediğimizde ne kadar esnek olabilmesidir. OOP burada devreye girmektedir.
+            Personel prs = new Personel(); // nesne ürettik.
+            prs.Adi = textBox1.Text;
+            prs.Soyadi = textBox2.Text;
+            prs.SicilNo = textBox3.Text;
+            prs.Maas = numericUpDown1.Value;
+
+            personelform.PersonelEkle(prs);
+
+
+            textBox1.Text = textBox2.Text = textBox3.Text = "";
+            numericUpDown1.Value = 0;
+            textBox1.Focus();
+        }
+        void PersonelAl( Personel yenikisi)
+        { 
+            //Personel alımı ile alakalı metotdur.
+            //veritabanına kayıt yapan kodlar burada yer alacakıtr.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            personelform.Show();
+        }
+    }
+}
---
+++ OOP/OOP/Personel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Random
+    {
+        int Next() { return 0; }
+        int Next(int max) { return 0; }
+        int Next(int min, int max) { return 0; }
+    }
+
+    public class Personel { 
+        /*Access  Modiiers: Sınıfın içindeki elemanlara nerelerden erişebileceğini belirleyen elemanlara denir. 
+          *Private -sadece tanımlandığı sını içinde erişilebilir yapar. Sınıf içinde access modiier yazılmadıysa private kabul edilir.
+          *Protected -miras alınan sınılardan erişelebilmeyi sağlar. 
+          *internal - bulunduğu  proje içinden erişelebilmeyi sağlar.
+          *protected internal -hem miraas alanlar hemde proje içindeki kullanılan yerlerde erişebilir olur. 
+          *public -umumi demektir her yerden erişilebilir. Bütün izinleri açar.
+        */
+
+        //Bir nesne tasarlandığında default private 'dır.
+        //Persone ile ilgili işlemler burada yapılacaktır.
+        //Class içinde tanımlanırsa alan field olur. Nesne ile ilgili alanlara field denir.
+        //class ile üretilmiş her bir nesnein özelliği olacaktır.
+        internal string Adi;
+        internal string Soyadi;
+        internal string SicilNo;
+        internal decimal Maas;
+        internal string TCKimlik;
+        DateTime IseGirisTarihi;
+
+        void IseAl()
+        {
+            //veritabanına kayıt yapan kodlar burada yer alacaktır.
+        }
+
+        decimal KıdemTazminatiHesapla()
+        {
+            //hesaplama kodları yer alacaktır.
+
+            return 0;
+        }
+
+        public void IstenCikar()
+        {
+            decimal para = KıdemTazminatiHesapla();
+            //veritabanında işten çıkarma ile ilgili işlemler yapılacaktır.
+        }
+
+    }
+}
