55214640791c7acae978be70b8002f464f03acd0
adabusra <busraa.adaa@gmail.com>
2017-05-30 23:27:07 +0300

    Polmorhism

---
+++ Polymorhism/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Polymorhism
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci ogr = new Ogrenci();
+            ogr.Adi = textBox1.Text;
+            ogr.Soyadi = textBox2.Text;
+
+            MessageBox.Show(ogr.ToString());
+            listBox1.Items.Add(ogr);
+
+
+        }
+    }
+}
---
+++ Polymorhism/Ogrenci.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorhism
+{
+    class Ogrenci: Insan
+    {
+        public decimal Vize { get; set; }
+        public decimal Final { get; set; }
+        //virtual olan değişkenleri oluşturmaak için kullanılan metoddur.
+        public override string ToString()
+        {
+            return Adi + " " + Soyadi;
+        }
+
+    }
+}
